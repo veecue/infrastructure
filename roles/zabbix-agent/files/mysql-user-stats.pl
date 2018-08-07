026480fd289fdf58b406e82d07789a5dbb96caa1
Florian Pritz <bluewind@xinu.at>
2018-08-07 14:46:07 +0200

    WIP: Monitor mysql table stats
    TODO this still needs to give zabbix SELECT permissions to the tables,
    otherwise the table statistics are not available. Do we want zabbix to
    be able to read everything? Not sure yet, leaning towards no.
    
    I don't want to lose the other changes though so I'm commiting this here
    for now.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/zabbix-agent/files/mysql-user-stats.pl
+++ roles/zabbix-agent/files/mysql-user-stats.pl
@@ -7,27 +7,30 @@ use JSON;
 
 my $config = $ARGV[0];
 my $mode = $ARGV[1];
-my $user = $ARGV[2];
-
-if (!($mode eq "userstat" or $mode eq "discover")) {
-	print STDERR "mode is not userstat or discover\n";
-	die;
-}
-
-if ($mode eq "userstat" and not defined $user) {
-	print STDERR "userstat requires user to be defined\n";
-	die;
-}
 
 my $db = DBI->connect("DBI:mysql:mysql_read_default_file=$config");
 
 if ($mode eq "userstat") {
+	my $user = $ARGV[2] // die "user not defined";
 	$db->{FetchHashKeyName} = "NAME_lc";
 	print encode_json($db->selectrow_hashref("select * from information_schema.user_statistics where user = ?", undef, $user));
+
+} elsif ($mode eq "tablestats") {
+	my $schema = $ARGV[2] // die "schema not defined";
+	my $table = $ARGV[3] // die "table not defined";
+	$db->{FetchHashKeyName} = "NAME_lc";
+	print encode_json($db->selectrow_hashref("select * from information_schema.table_statistics where table_schema = ? and table_name = ?", undef, $schema, $table));
+
+} elsif ($mode eq "tablestats_discover") {
+	print encode_json({
+			data => $db->selectall_arrayref("select table_schema '{#SCHEMA}', table_name '{#TABLENAME}' from information_schema.table_statistics", {Slice=>{}})
+		});
+
 } elsif ($mode eq "discover") {
 	print encode_json({
 			data => $db->selectall_arrayref("select user '{#USERNAME}' from information_schema.user_statistics", {Slice=>{}})
 		});
+
 } else {
 	die "unhandeled mode";
 }
