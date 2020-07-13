790790eebd9988f77c7a8aff8bcd450555927d73
Jelle van der Waa <jelle@archlinux.org>
2020-07-13 21:09:18 +0200

    Add borg backup missing alert

--- roles/prometheus/files/node-textcollector.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Atomic write for prometheus node exporter
-/usr/local/bin/arch-textcollector.sh > {{ prometheus_textfile_dir}}/pacman.prom.$$
-mv {{ prometheus_textfile_dir }}/pacman.prom.$$ {{ prometheus_textfile_dir }}/pacman.prom
